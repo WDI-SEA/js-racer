a7811927c8d8a2b034ff0b0721909a4520af5a52
ortizk <ortizk@live.com>
2018-03-23 20:39:16 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const playerOne = document.getElementById("car-one");
+const playerTwo = document.getElementById("car-two");
+
+var distance = 0;
+
+// const moveAlong = (key) => {
+	key.addEventListener('keydown')
+		if(key = 70) {
+			console.log(key)
+			// distance = playerOne.setAttribute("style", "left += 5");
+	};
+		// if(key === 74) {
+
+		// }
+// };
+
+// }
+
+// moveAlong();
+
