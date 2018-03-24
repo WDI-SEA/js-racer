1fbd8aeebe434b47e43cda4bc17c79d83afae9bc
ortizk <ortizk@live.com>
2018-03-24 12:35:55 -0700

    those damn cars are moving

--- scripts.js
+++ scripts.js
@@ -1,20 +1,38 @@
 const playerOne = document.getElementById("car-one");
 const playerTwo = document.getElementById("car-two");
 
-var distance = 0;
+var distance = 75;
 
-const moveAlong = (x) => {
-	playerOne.addEventListener('keydown', (distance) => {
-		if(x.keyCode === 70) {
-			distance = playerOne.setAttribute("style", "left += 5");
-	};
-	playerTwo.addEventListener('keydown', (distance) => {
-		if(x.keyCode === 74) {
-			distance = playerTwo.setAttribute("style", "left += 5");
+
+	document.addEventListener('keydown', (e) => {
+		if(70 === e.keyCode) {
+			distance += 10;
+			playerOne.style.left = distance + 'px';
 		};
-});
-});
-}
+	})
 
-moveAlong();
+	document.addEventListener('keydown', (e) => {
+		if(74 === e.keyCode) {
+			distance += 10;
+			playerTwo.style.left = distance + 'px';
+		};
+	})
+
+
+
+
+
+
+//const moveAlong = (e) => {
+		//keep the information of the key pressed to pass through the if loop to see if it's the key we're looking for(the purpose for the e)
+	//	if(e.keyCode === 70) {
+	//playerTwo.addEventListener('keydown', (e) => {
+	//	if(e.keyCode === 74) {
+	//		distance = playerTwo.setAttribute("style", "left += 5");
+	//	};
+//});
+//};
+//}
+
+//moveAlong();
 
