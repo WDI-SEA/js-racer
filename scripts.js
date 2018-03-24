3256dd77ea05f2ec507f6f111618c3e5a8972e1c
ortizk <ortizk@live.com>
2018-03-23 21:54:52 -0700

    second commit

--- scripts.js
+++ scripts.js
@@ -3,18 +3,18 @@ const playerTwo = document.getElementById("car-two");
 
 var distance = 0;
 
-// const moveAlong = (key) => {
-	key.addEventListener('keydown')
-		if(key = 70) {
-			console.log(key)
-			// distance = playerOne.setAttribute("style", "left += 5");
+const moveAlong = (x) => {
+	playerOne.addEventListener('keydown', (distance) => {
+		if(x.keyCode === 70) {
+			distance = playerOne.setAttribute("style", "left += 5");
 	};
-		// if(key === 74) {
+	playerTwo.addEventListener('keydown', (distance) => {
+		if(x.keyCode === 74) {
+			distance = playerTwo.setAttribute("style", "left += 5");
+		};
+});
+});
+}
 
-		// }
-// };
-
-// }
-
-// moveAlong();
+moveAlong();
 
