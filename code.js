1da8f8b982bad255747da077570a4a8cb09fe5e2
cippero <gilwein@gmail.com>
2018-03-25 14:53:16 -0700

    added scores and restart still in progress

--- code.js
+++ code.js
@@ -1,32 +1,36 @@
-const car1 = document.getElementById("car1");
-const car2 = document.getElementById("car2");
+const carRed = document.getElementById("car1");
+const carBlue = document.getElementById("car2");
+const finish = document.getElementById("finishLine");
+const restartBtn = document.getElementById("restartButton");
 let playing = true;
+let redPos = 120;
+let bluePos = 100;
+let scoreRed =  document.getElementById("scoreRed");
+let scoreBlue = document.getElementById("scoreBlue");
 
-const getRollingRed = () => {
-	let moveCar = 20;
+const getRollingRed = (redPos) => {
 	const car = "Red";
 	document.onkeyup = (event) => {
 		if(playing){
 			if(event.keyCode === 37){
-				moveCar += 20;
-				car1.style.left = moveCar + "px";
-				console.log(moveCar, car);
-				checkWin(moveCar, car);
+				redPos += 20;
+				carRed.style.left = redPos + "px";
+				//console.log(moveCar, car);
+				checkWin(redPos, car);
 			}
 		}
 	}
 }
 
-const getRollingBlue = () => {
-	let moveCar = 0;
+const getRollingBlue = (bluePos) => {
 	const car = "Blue";
 	window.onkeyup = (event) => {
 		if(playing){
 			if(event.keyCode === 39){
-				moveCar += 20;
-				car2.style.left = moveCar + "px";
-				console.log(moveCar, car);
-				checkWin(moveCar, car);
+				bluePos += 20;
+				carBlue.style.left = bluePos + "px";
+				//console.log(moveCar, car);
+				checkWin(bluePos, car);
 			}
 		}
 	}
@@ -42,12 +46,36 @@ const getWidth = () => {
   );
 }
 
-const checkWin = (moveCar, car) => {
-	if (moveCar > getWidth()*0.97){
-		alert(`${car} player wins!`);
+const checkWin = (carPos, car) => {
+	//console.log(window.getComputedStyle(document.getElementById("finishLine")).left);
+	if (carPos > getWidth()*0.88) {
+	 //if (moveCar > finish.style.left) {
+		//alert(`${car} player wins!`);
 		playing = false;
+		updateScore(car);
 	}
 }
 
-getRollingRed();
-getRollingBlue();
+const updateScore = (car) => {
+	if(car === "Red") {
+		let score = Number(scoreRed.innerHTML)+1;
+		scoreRed.innerHTML = score;
+	} else if (car === "Blue") {
+		let score = Number(scoreBlue.innerHTML)+1;
+		scoreBlue.innerHTML = score;
+	}
+}
+
+const restartGame = () => {
+	carRed.style.left = redPos;
+	carBlue.style.left = bluePos;
+	getRollingRed(redPos);
+	getRollingBlue(bluePos);
+	playing = true;
+}
+
+restartBtn.addEventListener("click", restartGame);
+
+restartGame();
+getRollingRed(redPos);
+getRollingBlue(bluePos);
